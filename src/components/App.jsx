a52b3d29eadbaabf7283e8d99be72bed87d8e577
Angelisx <43751883+Angelisx@users.noreply.github.com>
2021-02-01 17:53:04 -0500

    GOal 2 accomplished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,37 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
-function Question(props){
-    //return "<question" + val + "/>"
-    return <p> {props.text} </p>;
+function Question(props) {
+  //return "<question" + val + "/>"
+  return (
+    <div>
+      <p> {props.text} </p>
+      <Answer text={props.text} />
+    </div>
+  );
 }
 function App() {
   let currentquestion = 0;
-  return <div className="app"><Question text={data[currentquestion].question.text}/></div>;
-  
+  return (
+    <div className="app">
+      <Question text={data[currentquestion].question.text} />
+      <Question choices={data[currentquestion].question.choices} />
+      <NextQuestion />
+    </div>
+  );
 }
-
+function NextQuestion() {
+  return (
+    <div className="app">
+      <button> press me to move on </button>
+    </div>
+  );
+}
+function Answer(props) {
+  return (
+    <div>
+      <p> {props.text}</p>
+    </div>
+  );
+}
 export default App;
